84981d22b821b3f62723a5a050609b40a47d6871
Tommy <t@tommyhoang.com>
2018-08-21 10:46:55 -0700

    add comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,8 @@ class ArticlesController < ApplicationController
 
 	def show
 		@articles = Article.find(params[:id])
+		@comment = Comment.new
+		@comment.article_id = @articles.id
 	end
 
 	def new
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_162706) do
+ActiveRecord::Schema.define(version: 2018_08_17_232222) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.integer "article_id"
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
